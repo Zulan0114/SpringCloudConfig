bf6399b7e92f1d19a8d28f4f961042b1fb7d3108
Zulan0114 <dengzulan@163.com>
2018-12-14 15:04:03 +0800

    Eureka client gets service url from Eureka server by service name.

--- config-client/src/main/java/com/example/configclient/web/ConfigClientController.java
+++ config-client/src/main/java/com/example/configclient/web/ConfigClientController.java
@@ -1,13 +1,19 @@
 package com.example.configclient.web;
 
+import com.example.configclient.service.ConfigClientService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RefreshScope
 public class ConfigClientController {
+
+    @Autowired
+    ConfigClientService configClientService;
 
     @Value("${foo}")
     String foo;
@@ -15,6 +21,12 @@ public class ConfigClientController {
     @RequestMapping(value = "/hi")
     public String hi() {
         return foo;
+    }
+
+    @RequestMapping(value = "/url")
+    public String serviceURL(@RequestParam("applicationName") String applicationName) {
+        String url = configClientService.serviceUrl(applicationName);
+        return url;
     }
 
 }
